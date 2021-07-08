f7c8870c175bdd48936ffe65373fc29c54ad1bf1
MatKie <mtth.kiesel@gmail.com>
2021-07-08 18:09:54 +0100

    Towards regressing full minima profile
    added constraints to bilayer vesicle: at least 1 surf. inside,
    outer radius at least length of surfactant
    added test to check when bilayer vesicles should be favoured
    over spheres (needs refinement and solving).
    Added figure 6 from reinhardt et al.

--- MTM/mt_system.py
+++ MTM/mt_system.py
@@ -14,20 +14,26 @@ class MTSystem(object):
     Following Enders and Haentzschel, 1998
     """
 
-    def __init__(self):
+    types = {
+        "spheres": SphericalMicelle,
+        "globular": GlobularMicelle,
+        "rodlike": RodlikeMicelle,
+        "vesicles": BilayerVesicle,
+    }
+
+    def __init__(
+        self, T=298.15, m=8, spheres=True, globular=False, rodlike=True, vesicles=True
+    ):
         self.free_energy_minimas = None
         self.free_energy_types = None
+        self.wanted_keys = None  # set by next function
+        self.micelles = self.get_micelles(
+            T, m, spheres=spheres, globular=globular, rodlike=rodlike, vesicles=vesicles
+        )
+        self.T = T
+        self.m = m
 
-    def get_free_energy_minimas(
-        self,
-        T=298.15,
-        m=8,
-        spheres=True,
-        globular=False,
-        rodlike=True,
-        vesicles=True,
-        **kwargs
-    ):
+    def get_free_energy_minimas(self, T=None, m=None, **kwargs):
         """
         Calculate free energy of various micelle shapes over all
         aggregatin numbers.
@@ -35,9 +41,63 @@ class MTSystem(object):
         Parameters
         ----------
         T : float, optional
-            Temperature in Kelving, by default 298.15
+            Temperature in Kelvin, by default from system temp
         m : int, optional
-            Tail lenght in number of carbon atoms, by default 8
+            Tail lenght in number of carbon atoms, by default from system
+
+        Returns
+        -------
+        np.array
+            array of self.free_energy_minimas. Gives the minimal free
+            energy at every aggregation number (starting from one in the
+            array)
+        """
+        # n is the maximal aggregation number
+        n = 400
+        if T is not None or m is not None:
+            # It's either updated or the same...
+            self.micelles = self.get_micelles(
+                T, m, **{k: True for k in self.wanted_keys}
+            )
+
+        sizes = np.arange(1, n)
+        chempots = np.zeros((n - 1, len(self.wanted_keys)))
+
+        # Loop over sizes and micelle types, if it's optimisable do it.
+        # If optimised geometry is not feasible give arbitrary high value.
+        for i, size in enumerate(sizes):
+            chempots[i, :] = self.get_chempots(size)
+
+        # Get the minima
+        self.free_energy_minimas = np.apply_along_axis(min, axis=1, arr=chempots)
+
+        # Get the types
+        chempots_and_minima = np.concatenate(
+            (chempots, np.reshape(self.free_energy_minimas, (-1, 1))), axis=1
+        )
+
+        self.free_energy_types = np.apply_along_axis(
+            lambda x: np.where(np.isclose(x[:-1], x[-1]))[0][0],
+            axis=1,
+            arr=chempots_and_minima,
+        )
+
+        self.free_energy_types = [self.wanted_keys[i] for i in self.free_energy_types]
+
+        return self.free_energy_minimas
+
+    def get_micelles(
+        self, T, m, spheres=False, globular=False, rodlike=False, vesicles=False, *args
+    ):
+        """
+        Set the micelle attribute to include all the micelles we set to True.
+
+        Parameters
+        ----------
+        T : float
+            temperature of system.
+        m : int
+            Number of carbon beads in tail.
         spheres : bool, optional
             Whether or not to take spherical micelles into account,
             by default True
@@ -53,60 +113,47 @@ class MTSystem(object):
 
         Returns
         -------
-        np.array
-            array of self.free_energy_minimas. Gives the minimal free
-            energy at every aggregation number (starting from one in the
-            array)
+        list of MTM.micelle micelles objects
+            only the ones which were set to True
         """
-        types = {
-            "spheres": SphericalMicelle,
-            "globular": GlobularMicelle,
-            "rodlike": RodlikeMicelle,
-            "vesicles": BilayerVesicle,
-        }
-        # n is the maximal aggregation number
         # g_0 effectively will not be used.
         g_0 = 30
-        n = 400
-
         # Get all the keys we put 'true' for and have a class in types in
         wanted_types = {
-            k: v for k, v in locals().items() if k in types.keys() and v is True
+            k: v
+            for k, v in locals().items()
+            if k in MTSystem.types.keys() and v is True
         }
         wanted_keys = [k for k in wanted_types.keys()]
 
-        sizes = np.arange(1, n)
-        chempots = np.zeros((n - 1, len(wanted_keys)))
+        micelles = []
+        for key in wanted_keys:
+            micelles.append(MTSystem.types.get(key)(g_0, T, m, throw_errors=False))
 
-        # Loop over micelle types and sizes, if it's optimisable do it.
-        # If optimised geometry is not feasible give arbitrary high value.
-        for i, key in enumerate(wanted_keys):
-            micelle = types.get(key)(g_0, T, m, throw_errors=False)
-            for ii, size in enumerate(sizes):
-                micelle.surfactants_number = size
-                if hasattr(micelle, "optimise_radii"):
-                    micelle.optimise_radii(hot_start=False)
-                if micelle.geometry_check or micelle.geometry_check is None:
-                    chempots[ii, i] = micelle.get_delta_chempot()
-                else:
-                    chempots[ii, i] = 101
+        self.wanted_keys = wanted_keys
+        self.micelles = micelles
+        return self.micelles
 
-        # Get the minima
-        self.free_energy_minimas = np.apply_along_axis(min, axis=1, arr=chempots)
-        chempots_and_minima = np.concatenate(
-            (chempots, np.reshape(self.free_energy_minimas, (-1, 1))), axis=1
-        )
+    def get_chempots(self, size):
+        """
+        Get all micelle chempot differences for a given size.
 
-        # Get the types
-        self.free_energy_types = np.apply_along_axis(
-            lambda x: np.where(np.isclose(x[:-1], x[-1]))[0][0],
-            axis=1,
-            arr=chempots_and_minima,
-        )
-
-        self.free_energy_types = [wanted_keys[i] for i in self.free_energy_types]
-
-        return self.free_energy_minimas
+        Parameters
+        ----------
+        micelles : list of micelletypes
+        size : float
+            micelle aggregation size.
+        """
+        chempots = np.zeros((len(self.micelles)))
+        for i, micelle in enumerate(self.micelles):
+            micelle.surfactants_number = size
+            if hasattr(micelle, "optimise_radii"):
+                micelle.optimise_radii(hot_start=False)
+            if micelle.geometry_check or micelle.geometry_check is None:
+                chempots[i] = micelle.get_delta_chempot()
+            else:
+                chempots[i] = 101
+        return chempots
 
     def get_monomer_concentration(self, surfactant_conc, *args):
 
